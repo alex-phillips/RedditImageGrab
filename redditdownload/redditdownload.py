8fd57657bfe1b4cf91e8401436f7c0c83cefa70d
alex-phillips <ahp118@gmail.com>
2017-12-07 14:59:19 -0500

    additional logging

--- redditdownload/redditdownload.py
+++ redditdownload/redditdownload.py
@@ -421,7 +421,7 @@ def main():
                     comment_album_urls = []
 
                     try:
-                        time.sleep(2)
+                        time.sleep(4)
                         req = Request(comments_url)
                         json = urlopen(req).read()
                         data = JSONDecoder().decode(json)
